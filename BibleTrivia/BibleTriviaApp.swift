a278a711e634604ef5d86a2683e66cc37d2f71aa
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2024-10-08 17:43:12 +0300

    Initial Commit

---
+++ BibleTrivia/BibleTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleTriviaApp.swift
+//  BibleTrivia
+//
+//  Created by John-Mark Iliev on 8.10.24.
+//
+
+import SwiftUI
+
+@main
+struct BibleTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
