721eb09faa7f41527c7a266255f4d80de40554f1
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2024-10-08 18:03:41 +0300

    added a font, plist, colors, and custom color file

---
+++ BibleTrivia/Views/Supporting Files/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  BibleTrivia
+//
+//  Created by John-Mark Iliev on 5.10.24.
+//
+
+import SwiftUI
+
+extension Color {
+    
+    static let BTBackground: Color = Color("Background")
+    static let BTPrimary: Color = Color("Primary")
+    static let BTSecondary: Color = Color("Secondary")
+    static let BTDarkGray: Color = Color("Dark Gray")
+    static let BTLightGray: Color = Color("Light Gray")
+    static let BTBlack: Color = Color("Black")
+    static let BTStroke: Color = Color("Stroke")
+}
